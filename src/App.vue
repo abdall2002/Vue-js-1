0c29451956127b54a7424981d564f1ebe258c045
unknown <abdallahaliahmed517@gmail.com>
2024-08-31 12:11:17 +0300

    vvv

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,7 @@
         <app-header />
         <!-- <app-footer/> -->
         <div class="container">
-            <Cars />
-            <!-- {{ cars[2].model }} -->
-            <hr />
-            <!-- <button @click="updateCar">Change the brand</button> -->
-            <CarBrands>
-                <div>Content at the top</div>
-                <template v-slot:brands>
-                    <ul>
-                        <li v-for="(brand, index) in brands" :key="index">
-                            {{ brand }}
-                        </li>
-                    </ul>
-                </template>
-                <template v-slot:other>
-                    <div>Some other content</div>
-                </template>
-                <strong>Default slots</strong>
-            </CarBrands>
+
         </div>
     </div>
 </template>
@@ -32,23 +15,6 @@
     import CarBrands from '@/components/Cars/brands.vue'
     import { reactive, provide } from 'vue';
     
-    const brands = reactive(['Mazda', 'Honda', 'Renault'])
-
-    const cars = reactive([
-        { model: 'F9', brand: 'Ferrari' },
-        { model: '911', brand: 'Porsche' },
-        { model: 'Fiesta', brand: 'Ford' }
-    ])
-
-
-    
-    const updateCar = () => {
-        cars[0].brand = 'fiat';
-    }
-provide('cars', {
-    cars,
-    updateCar
-})
 </script>
 
 <style>
